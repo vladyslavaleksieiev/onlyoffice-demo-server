e71c4a5cbd11b663e787f609086d57eef291a1af
Vladyslav Aleksieiev <vladyslav.aleksieiev@gmail.com>
2026-03-11 10:47:19 +0000

    feat: add events to onlyoffice plugin

--- plugins/shortcut-plugin/scripts.js
+++ plugins/shortcut-plugin/scripts.js
@@ -3,41 +3,26 @@
   const MENU_ITEM_ID = "open-react-side-modal";
 
   window.Asc.plugin.init = function () {
-    // For newer APIs, attachEditorEvent is preferred from 8.2+
     if (window.Asc.plugin.attachEditorEvent) {
       window.Asc.plugin.attachEditorEvent("onContextMenuShow", onContextMenuShow);
     } else {
       window.Asc.plugin.attachEvent("onContextMenuShow", onContextMenuShow);
     }
-
-    document.addEventListener("keydown", (e) => {
-      if (e.key === "<") {
-        window.parent.postMessage({
-          source: "onlyoffice-plugin",
-          type: "KEYBOARD_SHORTCUT",
-          data: {
-            key: "keyboard-<"
-          }
-        }, "*");
-      }
-    });
   };
 
   function onContextMenuShow(options) {
-    // You can inspect options to show item only in certain cases
-    // e.g. text selection, image, paragraph, etc.
+    const items = {
+      guid: PLUGIN_GUID,
+      items: [
+        {
+          id: MENU_ITEM_ID,
+          text: "Open side modal",
+          icons: "icons/icon16.png"
+        }
+      ]
+    };
 
-    window.Asc.plugin.executeMethod("AddContextMenuItem", [[
-      {
-        guid: PLUGIN_GUID,
-        items: [
-          {
-            id: MENU_ITEM_ID,
-            text: "Open side modal"
-          }
-        ]
-      }
-    ]]);
+    window.Asc.plugin.executeMethod("AddContextMenuItem", [items]);
   }
 
   window.Asc.plugin.onContextMenuClick = function (id) {
